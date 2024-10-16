910bf4e0221050928d6fcb9933e6478230ee40d5
sparksccoe <114624034+sparksccoe@users.noreply.github.com>
2024-10-16 05:32:58 +0000

    added more hide styles

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,37 @@ import pandas as pd
 import plotly.express as px
 from datetime import datetime
 
-<style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 image = Image.open('data_adventures_logo.png')
