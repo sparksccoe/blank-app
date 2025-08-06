9f17427a42687d2ee631b58a2d46227b58fda772
sparksccoe <114624034+sparksccoe@users.noreply.github.com>
2025-08-06 20:43:53 +0000

    changed selected_creature_obj

--- DA_app.py
+++ DA_app.py
@@ -563,7 +563,7 @@ if "best_match" in st.session_state:
                 None
             )
 
-            if selected_creature_obj:
+            if selected_creature_obj is not None:
                 st.markdown("### 🎯 Which music task would you like your creature to complete?")
 
                 music_tasks = [
